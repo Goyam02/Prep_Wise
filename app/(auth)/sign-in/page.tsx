f82ab5436bc6f68df2f3a404c9fca149f25785c3
GOYAM JAIN <goyamjain02@gmail.com>
2025-05-18 01:46:34 +0530

    added components

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+import AuthForm from "@/components/AuthForm";
+
+const Page = () =>{
+    return <AuthForm tye="sign-in" />
+}
+
+export default Page
---
+++ components/AuthForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AuthForm = () =>{
+    return (
+        <div>Page</div>
+    )
+}
+
+export default AuthForm
