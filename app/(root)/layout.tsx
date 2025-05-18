86928bf356fb80b824038807f846cb72aaf0ae79
GOYAM JAIN <goyamjain02@gmail.com>
2025-05-18 14:31:45 +0530

    added homepage components and sections

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,18 @@
 import {ReactNode} from 'react'
+import Link from "next/link";
+import Image from "next/image";
 
 const RootLayout = ({ children }: {children :ReactNode}) =>{
     return (
-        <div>{children}</div>
+        <div className="root-layout">
+            <nav>
+                <Link href="/" className="flex gap-2 items-center">
+                    <Image src="/logo.svg" alt="logo" width={38} height={32}></Image>
+                    <h2 className="text-primary-100">Prep.AI</h2>
+                </Link>
+            </nav>
+            {children}
+        </div>
     )
 }
 
---
+++ components/InterviewCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InterviewCard = () => {
+    return (
+        <div>
+            InterviewCard
+
+        </div>
+    );
+};
+
+export default InterviewCard;
